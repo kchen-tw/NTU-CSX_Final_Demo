9cdb8505c3459147ae3dd652ce17030f1782fda5
Kun Chen <kchen.ntu@gmail.com>
2017-02-13 23:37:01 +0800

    update

--- js/index-vue.js
+++ js/index-vue.js
@@ -1,42 +1,57 @@
-var project_card_group = new Vue({
-    el: '#project_card_group',
-    data: {
-        projects: [],
-    },
-    beforeCreate: function() {
-        var options = {
-            api: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
-            sheet: 'team_list',
-            filter: {
-                demo: true
-            }
-        };
-        $('.loader-wrapper').removeClass('hide');
-        queryData(options, function(res) {
-            project_card_group.projects = res.data;
-        });
-    },
-    updated: function() {
-        $('.loader-wrapper').addClass('hide');
-    }
-});
+// 當網頁載入完開始javascript
+$(function () {
 
-var teammate_card_group = new Vue({
-    el: '#teammate_card_group',
-    data: {
-        teammate: [],
-    },
-    beforeCreate: function() {
-        var options = {
-            api: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
-            sheet: 'teammate'
-        };
-        $('.loader-wrapper').removeClass('hide');
-        queryData(options, function(res) {
-            teammate_card_group.teammate = res.data;
-        });
-    },
-    updated: function() {
-        $('.loader-wrapper').addClass('hide');
-    }
-});
+
+    var gsheet = new GSheet('https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec');
+
+    var loader = new Loader($('.loader-wrapper'));
+
+    var project_card_group = new Vue({
+        el: '#project_card_group',
+        data: {
+            projects: [],
+        },
+        beforeCreate: function () {
+            // $('.loader-wrapper').removeClass('hide');
+            loader.start();
+            let self = this;
+            gsheet.run({
+                sheet: 'team_list',
+                filter: {
+                    demo: true
+                },
+                done: function (response) {
+                    self.projects = response.data;
+                }
+            });
+        },
+        updated: function () {
+            loader.end();
+            // $('.loader-wrapper').addClass('hide');
+        }
+    });
+
+    var teammate_card_group = new Vue({
+        el: '#teammate_card_group',
+        data: {
+            teammate: [],
+        },
+        beforeCreate: function () {
+
+            loader.start();
+            let self = this;
+            gsheet.run({
+                sheet: 'teammate',
+                done: function (response) {
+                    self.teammate = response.data;
+                }
+            });            
+
+        },
+        updated: function () {
+            loader.end();
+            // $('.loader-wrapper').addClass('hide');
+        }
+    });
+
+})
